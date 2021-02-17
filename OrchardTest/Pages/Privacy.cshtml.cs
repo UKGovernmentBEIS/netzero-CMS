d4f52f9af4f970bab50394fc374da0b4b291c4e5
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-02-17 07:57:38 +0000

    Changed Solution name to OrchardCMS

--- OrchardTest/Pages/Privacy.cshtml.cs
+++ OrchardTest/Pages/Privacy.cshtml.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OrchardTest.Pages
+namespace OrchardCMS.Pages
 {
     public class PrivacyModel : PageModel
     {
--- OrchardTest/Program.cs
+++ OrchardTest/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace OrchardTest
+namespace OrchardCMS
 {
     public class Program
     {
