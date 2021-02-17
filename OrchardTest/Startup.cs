7b8c0c260975f72d75e341e9fbc9b4294a3b5370
Lorenzo Lane <lorenzo.lane@beis.gov.uk>
2021-02-17 07:38:09 +0000

    Added GDS styling

--- OrchardTest/Startup.cs
+++ OrchardTest/Startup.cs
@@ -1,3 +1,4 @@
+using GovUk.Frontend.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -24,7 +25,8 @@ namespace OrchardTest
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddOrchardCms();
-          
+            services.AddGovUkFrontend();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
